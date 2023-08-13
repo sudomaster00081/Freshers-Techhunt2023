2d63212f8d537b7790552791a78887209ada3bdf
sudomaster00081 <ajayprasadd0008@gmail.com>
2023-08-13 22:32:47 +0530

    1st Mod 2

--- Challenge 1: Palindrome Checker.c
+++ Challenge 1: Palindrome Checker.c
@@ -6,10 +6,12 @@ int isPalindrome(char str[]);
 
 int main() {
     char input[100];
+//section 1 end
 
     // Section 2 User Input
     printf("Enter a string: ");
     scanf("%s", input);
+//section 2 end
 
     // Section 3 Palindrome Check
     if (isPalindrome(input)) {
@@ -20,11 +22,14 @@ int main() {
 
     return 0;
 }
+//section 3 end
 
 // Section 4 Palindrome Function
 int isPalindrome(char str[]) {
     int left = 0;
     int right = strlen(str) - 1;
+//section 4 end
+
 
     // Section 5 Palindrome Check Loop
     while (left < right) {
@@ -37,7 +42,7 @@ int isPalindrome(char str[]) {
     return 1; // Palindrome
 }
 
-
+//section 5 end
 
 // Fake Section 1
 int isPalindrome(char str[]) {
